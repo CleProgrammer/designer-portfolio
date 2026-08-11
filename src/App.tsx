f8638a705b6918e27f4c0deaaa251204417e85cb
CleProgrammer <clebsonmengao@hotmail.com>
2026-08-11 17:40:31 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -381,7 +381,7 @@ function App() {
                   <img
                     id="Img7"
                     src={Img7}
-                    alt="Arte de evento Wesley Safadão - Clebson Designer Gráfico Social Media"
+                    alt="Arte de evento - Clebson Designer Gráfico Social Media"
                     loading="lazy"
                   />
                   <a href='https://clebsondesigner.com.br/flyer-para-evento' target='blank' className="open-hint" id="Img7">Abrir página</a>
@@ -390,19 +390,19 @@ function App() {
                   <img
                     id="Img8"
                     src={Img8}
-                    alt="Flyer de evento jogo do Flamengo"
+                    alt="Designer Gráfico para Eventos - Ana Castela"
                     loading="lazy"
                   />
-                  <a href='https://clebsondesigner.com.br/designer-grafico-esportivo' target='blank' className="open-hint" id="Img8">Abrir página</a>
+                  <a href='https://clebsondesigner.com.br/flyer-para-show' target='blank' className="open-hint" id="Img8">Abrir página</a>
                 </div>
                 <div className="portfolio-item" id="Img9" onClick={(e) => OpenModal(e)}>
                   <img
                     id="Img9"
                     src={Img9}
-                    alt="Designer Gráfico para Eventos - Ana Castela"
+                    alt="Flyer de evento jogo do Flamengo"
                     loading="lazy"
                   />
-                  <a href='https://clebsondesigner.com.br/flyer-para-show' target='blank' className="open-hint" id="Img9">Abrir página</a>
+                  <a href='https://clebsondesigner.com.br/designer-grafico-esportivo' target='blank' className="open-hint" id="Img9">Abrir página</a>
                 </div>
               </div>
             </div>
